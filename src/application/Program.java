636b71e62c93407abbdc23d92b87b73d096b903c
Lucia Rigoni Freire <luciarigoni@live.com>
2023-02-22 15:25:37 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,10 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);	
@@ -34,10 +38,10 @@ public class Program {
 				sc.nextLine();
 			}
 			
-//			catch (InputMismatchException e) {
-//				System.out.println(e.getMessage());
-//				sc.nextLine();
-//			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
